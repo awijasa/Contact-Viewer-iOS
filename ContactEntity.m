8ec2131b63b6a1c7ce76c00f04294978ac6daf63
Adrian Wijasa <awijasa@gmail.com>
2012-03-12 20:34:02 -0500

    1. Added the data model for Persistent Storage (Core Data). 2. Added Persistent ContactEntity class. 3. Added a new Contact screen.

---
+++ ContactEntity.m
@@ -0,0 +1,20 @@
+//
+//  ContactEntity.m
+//  ContactViewerIos
+//
+//  Created by Adrian Wijasa on 3/12/12.
+//  Copyright (c) 2012 Tiny Mission. All rights reserved.
+//
+
+#import "ContactEntity.h"
+
+
+@implementation ContactEntity
+
+@dynamic name;
+@dynamic title;
+@dynamic phone;
+@dynamic email;
+@dynamic twitterId;
+
+@end

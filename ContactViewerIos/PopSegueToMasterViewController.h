2793d288c8774d301613ddeb4dc504af2f55288c
Adrian Wijasa <awijasa@gmail.com>
2012-03-13 20:44:36 -0500

    Changed the naming of segue: PopSegue to PopSegueToMasterViewController, which is more accurate because the segue always brings the MasterViewController up.

---
+++ ContactViewerIos/PopSegueToMasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  PopSegue.h
+//  ContactViewerIos
+//
+//  Created by Adrian Wijasa on 3/13/12.
+//  Copyright (c) 2012 Tiny Mission. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PopSegueToMasterViewController : UIStoryboardSegue
+
+@end
